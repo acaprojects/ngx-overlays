f274cd0f91252c7c24c44055d0376bcbfb311890
Alex Sorafumo <alex@yuion.net>
2019-04-19 08:34:23 +1000

    Move library files to root src folder

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,7 +3,7 @@ import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
-import { OverlayModule } from '../../lib/src/public_api';
+import { OverlayModule } from '../lib/public_api';
 
 @NgModule({
   declarations: [
--- src/app/services/app.service.ts
+++ src/app/services/app.service.ts
@@ -1,5 +1,5 @@
 import { Injectable } from '@angular/core';
-import { OverlayService } from '../../../lib/src/public_api';
+import { OverlayService } from '../../lib/public_api';
 
 @Injectable({
     providedIn: 'root'
