00db5c98d04b51c26ae20cae7054e0385ff5f70a
Alex Sorafumo <alex@yuion.net>
2019-06-11 11:24:43 +1000

    fix: fix public api

--- projects/library/src/public-api.ts
+++ projects/library/src/public-api.ts
@@ -3,3 +3,15 @@
  */
 
 export * from './lib/library.module';
+
+export * from './lib/services/overlay.service';
+export * from './lib/classes/overlay-item.class';
+export * from './lib/interfaces/overlay.interfaces';
+
+export * from './lib/components/overlay-content/overlay-content.component';
+export * from './lib/components/tooltip-wrapper/tooltip-wrapper.component';
+
+export * from './lib/directives/context-item.directive';
+export * from './lib/directives/floating-text.directive';
+export * from './lib/directives/model.directive';
+export * from './lib/directives/tooltip.directive';
